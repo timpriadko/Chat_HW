2d4cb530698074de5e7745119be8fe99da8746d1
unknown <timpriadko@gmail.com>
2018-09-21 13:35:40 +0300

    add Dark Theme + chat toggle changes

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,35 @@
 'use strict';
 
+
 function toggleClass(clickTarget, classTarget, className){
-	var clickTarget = document.querySelector(clickTarget),
-		classTarget = document.querySelector(classTarget);
+	
+	var clickTargetVariable = document.querySelector(clickTarget),
+		classTargetVariable = document.querySelector(classTarget);
 
-		// document.onclick = function(e){
-		// 	if ((e.target !== classTarget) && (e.target !== clickTarget)){
-		// 		classTarget.classList.remove(className);
-		// 	};
-		// };
 
-		clickTarget.onclick = function(){
-			event.preventDefault();
-			classTarget.classList.toggle(className);
-		};
+		function clickListener(){
+			event.stopPropagation();
+			this.classList.toggle('toggle-active');
+			classTargetVariable.classList.toggle(className);
+		}
+
+		clickTargetVariable.addEventListener('click', clickListener);
+
+		document.addEventListener('click', function(e){
+
+			if (e.target.closest(classTarget)){
+				return;
+			}
+
+			if (classTargetVariable.classList.contains(className)) {
+				classTargetVariable.classList.remove(className);
+				clickTargetVariable.classList.remove('toggle-active');
+			}
+
+		});
 }
 
+
 toggleClass('.toggle-button','.sidebar', 'sidebar-open');
+toggleClass('.acc-status','.acc-menu','acc-menu-open');
+toggleClass('.toggle-box','body','night-mode');
